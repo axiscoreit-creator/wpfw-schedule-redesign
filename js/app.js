f4816b8c9f84a86a471ebdfb60adaa507b81e027
axiscoreit-creator <244848843+axiscoreit-creator@users.noreply.github.com>
2026-06-07 04:26:12 -0700

    positioning

--- js/app.js
+++ js/app.js
@@ -61,7 +61,7 @@ function buildGrid() {
   "5:00 AM", "6:00 AM", "7:00 AM", "8:00 AM", "9:00 AM", "10:00 AM",
   "11:00 AM", "Noon", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM",
   "5:00 PM", "6:00 PM", "7:00 PM", "8:00 PM", "9:00 PM",
-  "10:00 PM", "11:00 PM"
+  "10:00 PM", "11:00 PM", "12:00 AM", "1:00 AM", "2:00 AM", "3:00 AM", "4:00 AM"
 ];
   const query = searchInput.value.toLowerCase();
 
@@ -110,9 +110,12 @@ function buildGrid() {
           </button>
         `;
 
-        cell.querySelector(".program-card").addEventListener("click", (event) => {
-  showDetails(program, day, time);
-  showMobilePopover(program, day, time, event.currentTarget);
+       cell.querySelector(".program-card").addEventListener("click", (event) => {
+  if (window.innerWidth <= 700) {
+    showMobilePopover(program, day, time, event.currentTarget);
+  } else {
+    showDetails(program, day, time);
+  }
 });
       }
 
